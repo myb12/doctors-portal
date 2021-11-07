5829dea5824346e5bca61a1a6b13816c9e67d1ec
myb12 <mohammadyasinbappy@outlook.com>
2021-11-08 00:21:45 +0600

    Make an admin API is integrated

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+    console.log(email);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const user = { email }
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => { })
+    }
     return (
         <div>
-            <h1>Make an Admin</h1>
-        </div>
+            <h2>Make an Admin</h2>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    onBlur={handleEmail}
+                    label="Insert an email"
+                    variant="standard"
+                    type="email"
+                />
+                <br />
+                <Button type="submit" sx={{ mt: 1 }} variant="outlined">Make Admin</Button>
+
+            </form>
+        </div >
     );
 };
 
